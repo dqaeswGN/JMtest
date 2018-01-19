c5913448499b8227d36f6699acba5f6aaaf11212
dqaeswGN <15645430925@163.com>
2018-01-19 16:38:10 +0800

    aaaaa

--- src/MD5/Md5test.java
+++ src/MD5/Md5test.java
@@ -3,12 +3,26 @@ package MD5;
 import java.security.MessageDigest;
 
 public class Md5test {
-	public void md5test(){
+	public static  String  md5test(String text){
+		StringBuffer sb=new StringBuffer();
 		try{
 			MessageDigest md5 = MessageDigest.getInstance("md5");
+			byte[] data=md5.digest(text.getBytes());
+			
+			for(byte bak:data){
+				String value=Integer.toHexString(bak&0xff);
+				sb.append(value.length() == 1?"0"+value:value);
+			}
+			System.out.println(sb.toString());
 			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		return sb.toString(); 
+	}
+	
+	public static void main(String[] args){
+		md5test("hello");
+		
 	}
 }
